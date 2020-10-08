1bfc8c35e699daa05e8d864ef1099ab8406315b4
unknown <rajatsngpt7@gmail.com>
2020-10-08 21:33:08 +0530

    first commit

---
+++ src/Books.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import SearchArea from "./SearchArea";
+import BookList from "./BookList";
+import request from "superagent";
+
+class Books extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchField: "",
+    };
+  }
+
+  searchBook = (e) => {
+    e.preventDefault();
+    request
+      .get("https://www.googleapis.com/books/v1/volumes")
+      .query({ q: this.state.searchField })
+      .then((data) => {
+        console.log(data);
+        this.setState({ books: [...data.body.items] });
+      });
+  };
+
+  handleSearch = (e) => {
+    //console.log(e.target.value);
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchArea
+          searchBook={this.searchBook}
+          handleSearch={this.handleSearch}
+        />
+        <BookList books={this.state.books} />
+      </div>
+    );
+  }
+}
+
+export default Books;
